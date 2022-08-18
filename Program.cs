1ecf4db408e3fbfc5a537fbf34cd6cb06cd377d2
Olya <pradicha@mail.ru>
2022-08-18 22:05:53 +0300

    задача_5

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿//Console.WriteLine("Hello, World!");
 Console.Clear ( );
 
-// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 // Console.Write(" Введите первое число  -  ");
 // int a = Convert.ToInt32(Console.ReadLine());
@@ -20,19 +20,33 @@ Console.Clear ( );
 // }
 
 
-// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-//Console.Write( "Ведите три числа и мы скажем какое из них больше");
-Console.Write(" Введите первое число  -  ");
-int a = Convert.ToInt32(Console.ReadLine( ));
-Console.Write(" Введите второе число  -  ");
-int b = Convert.ToInt32(Console.ReadLine( ));
-Console.Write(" Введите третье число  -  ");
-int c = Convert.ToInt32(Console.ReadLine( ));
-int max = a;
-if (a > max) max =a;
-if (b > max) max = b;
-if (c > max) max = c;
-// Console.Write("max =  "); 
-// Console.WriteLine( max ); 
-//Аналогично 
-Console.WriteLine($"Максимальное число =  {max} " );
+// // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// //Console.Write( "Ведите три числа и мы скажем какое из них больше");
+// Console.Write(" Введите первое число  -  ");
+// int a = Convert.ToInt32(Console.ReadLine( ));
+// Console.Write(" Введите второе число  -  ");
+// int b = Convert.ToInt32(Console.ReadLine( ));
+// Console.Write(" Введите третье число  -  ");
+// int c = Convert.ToInt32(Console.ReadLine( ));
+// int max = a;
+// if (a > max) max =a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+// // Console.Write("max =  "); 
+// // Console.WriteLine( max ); 
+// //Аналогично 
+// Console.WriteLine($"Максимальное число =  {max} " );
+
+
+// // Задача 5: Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// Console.Write("Введите число ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int B = - N ;  // Объявляю новую переменную в которой сохраняю значение N с противоположным знаком
+
+// while ( B <= N) //Условный цикл, который работает до тех пор, пока B меньше или равно N
+// {
+//     Console.Write( B + "  "); // Выводим на экран значение B и пробел
+//     //Увеличиваем b на единицу
+//     B ++; 
+//      // аналогично b+ = 1 //B = B + 1;
+// }
