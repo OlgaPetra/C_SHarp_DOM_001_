5f7086951934c232b87ea75da0d29f69f0f6e7da
Olya <pradicha@mail.ru>
2022-08-18 22:02:33 +0300

    задача_4

--- Program.cs
+++ Program.cs
@@ -3,18 +3,36 @@ Console.Clear ( );
 
 // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-Console.Write(" Введите первое число  -  ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write(" Введите второе число  -  ");
-int b = Convert.ToInt32(Console.ReadLine());
-if (a > b)
-{
-    Console.WriteLine($"Больше число , { a }");
-    Console.WriteLine($"Меньшее число , { b }");
-}
+// Console.Write(" Введите первое число  -  ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write(" Введите второе число  -  ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a > b)
+// {
+//     Console.WriteLine($"Больше число , { a }");
+//     Console.WriteLine($"Меньшее число , { b }");
+// }
 
-else
-{
-    Console.WriteLine($"Большее число,{ b} ");
-    Console.WriteLine($"Меньшее число ,{ a}");
-}
+// else
+// {
+//     Console.WriteLine($"Большее число,{ b} ");
+//     Console.WriteLine($"Меньшее число ,{ a}");
+// }
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//Console.Write( "Ведите три числа и мы скажем какое из них больше");
+Console.Write(" Введите первое число  -  ");
+int a = Convert.ToInt32(Console.ReadLine( ));
+Console.Write(" Введите второе число  -  ");
+int b = Convert.ToInt32(Console.ReadLine( ));
+Console.Write(" Введите третье число  -  ");
+int c = Convert.ToInt32(Console.ReadLine( ));
+int max = a;
+if (a > max) max =a;
+if (b > max) max = b;
+if (c > max) max = c;
+// Console.Write("max =  "); 
+// Console.WriteLine( max ); 
+//Аналогично 
+Console.WriteLine($"Максимальное число =  {max} " );
